02aff6f02b831cacf32a9e0d1a98901778855c4b
omergulcicek <iletisim@omergulcicek.com>
2025-11-05 13:17:43 +0300

    refactor: simplify user data selection in useUsers hook
    - Removed the sleep function from the select method to enhance performance.
    - Streamlined the data selection process by directly slicing the user data array.

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { get } from "@/lib/api";
-import { sleep } from "@/lib/utils";
 
 import { UserType } from "@/types";
 
@@ -9,9 +8,6 @@ export function useUsers() {
   return useQuery({
     queryKey: ["users"],
     queryFn: ({ signal }) => get<UserType[]>("/users", { signal }),
-    select: async (data) => {
-      await sleep(2000);
-      return data.slice(0, 5);
-    }
+    select: (data) => data.slice(0, 5)
   });
 }
